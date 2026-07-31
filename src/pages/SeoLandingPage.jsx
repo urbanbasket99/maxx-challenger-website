317f094969c206a047629a9fb58ec643613f0b94
urbanbasket99 <srikanthreddyvan@gmail.com>
2026-07-31 18:30:03 +0530

    title page solved

--- src/pages/SeoLandingPage.jsx
+++ src/pages/SeoLandingPage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import seoPages from "../data/seoData";
 import { Link } from "react-router-dom";
 function SeoLandingPage() {
